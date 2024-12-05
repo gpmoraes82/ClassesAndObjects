e382617f7addd09e5a0edc65e71c8b9ed54da2b3
GabrielPM <gpmoraes82@gmail.com>
2024-12-04 21:37:58 -0300

    Update main

--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ class Program
         book1.author = "Liu Cixin";
         book1.pages = 302;
 
+        Book book2 = new Book();
+        book2.title = "The Warriors";
+        book2.author = "Sol Yurick";
+        book2.pages = 181;
+
         Console.WriteLine(book1.title);
+
+        Console.WriteLine(book2.title);
 
         Console.ReadLine();
     }
