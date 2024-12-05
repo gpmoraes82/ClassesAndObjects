f9616c1d9a6ad96d9d4151f45a2c8635cb0b4782
GabrielPM <gpmoraes82@gmail.com>
2024-12-05 15:36:50 -0300

    Update all Classes

--- Program.cs
+++ Program.cs
@@ -5,13 +5,15 @@ class Program
     static void Main(string[] args)
     {
 
+        Library library = new Library("Alexandria");
+
         Book book1 = new Book("3 Body Problem", "Liu Cixin", 302);
         Console.WriteLine(book1.title);
 
         Book book2 = new Book("The Warriors", "Sol Yurick", 181);
         Console.WriteLine(book2.title);
 
-        // Console.WriteLine($"Book for read if have time:{book1.bookSuggestion(book1, book2)}");
+        Console.WriteLine($"Book for read if have time: {library.bookSuggestion(book1, book2)}");
 
         Console.ReadLine();
     }
